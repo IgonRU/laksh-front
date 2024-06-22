eb11d1170175352e3ec3157fed91c43619501609
IgonRU <mtgsale@mail.ru>
2024-06-22 14:57:35 +0300

    Responsive design started

--- src/app/_layout/page-header/page-header.component.ts
+++ src/app/_layout/page-header/page-header.component.ts
@@ -1,12 +1,17 @@
 import {Component, input} from '@angular/core';
 import {LakshHeaderMenuComponent} from "../header-menu/header-menu.component";
 import {LakshHeaderMenuItem} from "../header-menu/header-menu-item.class";
+import {IgonResponsiveLayoutService} from "@igon/responsive-layout";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'laksh-page-header',
   standalone: true,
   imports: [
-    LakshHeaderMenuComponent
+    LakshHeaderMenuComponent,
+    MatButton,
+    MatIcon
   ],
   templateUrl: './page-header.component.html'
 })
@@ -14,4 +19,20 @@ export class LakshPageHeaderComponent {
 
   topMenu = input<LakshHeaderMenuItem[]>([]);
 
+  constructor(private responsiveService: IgonResponsiveLayoutService) {
+
+  }
+
+
+  showMenu(): boolean {
+    return this.responsiveService.isWeb()
+  }
+
+  showMenuToggler(): boolean {
+    return !this.showMenu();
+  }
+
+  showContacts(): boolean {
+    return this.responsiveService.isWeb() || this.responsiveService.isTablet();
+  }
 }
