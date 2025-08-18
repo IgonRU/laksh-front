c32f53823f69fefef29f9773579d5369975056c9
Igon <mtgsale@mail.ru>
2025-08-18 08:24:12 +0300

    fixed template

--- src/app/_layout/page-header/page-header.component.ts
+++ src/app/_layout/page-header/page-header.component.ts
@@ -5,7 +5,7 @@ import {IgonResponsiveLayoutService} from "@igon/responsive-layout";
 import {MatButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {NavStickyComponent} from "../nav-sticky/nav-sticky.component";
-import { LakshHeaderCenterMenuComponent } from "./header-conter-menu/header-center-menu.component";
+import { LakshHeaderCenterMenuComponent } from "./header-center-menu/header-center-menu.component";
 import { LakshHeaderLangSwitchComponent } from "./header-lang-switch/header-lang-switch.component";
 import { LakshHeaderLogoComponent } from "./header-logo/header-logo.component";
 
