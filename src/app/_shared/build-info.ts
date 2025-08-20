884f14ac00683464b607a21bb25a274639be85a0
Igon <mtgsale@mail.ru>
2025-08-20 09:51:44 +0300

    Footer legals row style fix

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-08-20T06:31:45.148Z",
-  "buildDate": "20.08.2025, 09:31:45",
-  "buildTimestamp": 1755671505148,
-  "commitHash": "fca02c2",
+  "buildTime": "2025-08-20T06:51:45.061Z",
+  "buildDate": "20.08.2025, 09:51:45",
+  "buildTimestamp": 1755672705061,
+  "commitHash": "c03055f",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.2.2"
+  "version": "0.2.3"
 };
 
 export default BUILD_INFO;
