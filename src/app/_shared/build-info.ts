baa6d476e6d18b10aafa3649c879a55fc6591691
Igon <mtgsale@mail.ru>
2025-08-21 19:18:49 +0300

    About us design improvements

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-08-21T08:08:36.665Z",
-  "buildDate": "21.08.2025, 11:08:36",
-  "buildTimestamp": 1755763716665,
-  "commitHash": "6ffcd76",
+  "buildTime": "2025-08-21T16:18:50.321Z",
+  "buildDate": "21.08.2025, 19:18:50",
+  "buildTimestamp": 1755793130321,
+  "commitHash": "2d4a8e1",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.2.5"
+  "version": "0.2.6"
 };
 
 export default BUILD_INFO;
