067fe56b9e1a97fe3f541c41338382258a4e254f
Igon <mtgsale@mail.ru>
2025-08-30 18:15:31 +0300

    Green-medium logo

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-08-30T13:41:34.415Z",
-  "buildDate": "30.08.2025, 16:41:34",
-  "buildTimestamp": 1756561294415,
-  "commitHash": "9007fb5",
+  "buildTime": "2025-08-30T15:15:32.169Z",
+  "buildDate": "30.08.2025, 18:15:32",
+  "buildTimestamp": 1756566932169,
+  "commitHash": "7ac362c",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.2.14"
+  "version": "0.2.15"
 };
 
 export default BUILD_INFO;
