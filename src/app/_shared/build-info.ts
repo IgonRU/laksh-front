9007fb5bf0967436317196845527fb3a7ee42541
Igon <mtgsale@mail.ru>
2025-08-30 16:20:49 +0300

    Services improvements

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-08-30T13:07:56.631Z",
-  "buildDate": "30.08.2025, 16:07:56",
-  "buildTimestamp": 1756559276631,
-  "commitHash": "ea7c81b",
-  "branch": "colors",
+  "buildTime": "2025-08-30T13:20:49.576Z",
+  "buildDate": "30.08.2025, 16:20:49",
+  "buildTimestamp": 1756560049576,
+  "commitHash": "f3da598",
+  "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.2.12"
+  "version": "0.2.13"
 };
 
 export default BUILD_INFO;
