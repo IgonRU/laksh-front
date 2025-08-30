7ac362cd43358440cc5f04797134ce6440ce8b57
Igon <mtgsale@mail.ru>
2025-08-30 16:41:34 +0300

    logo size fix

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-08-30T13:20:49.576Z",
-  "buildDate": "30.08.2025, 16:20:49",
-  "buildTimestamp": 1756560049576,
-  "commitHash": "f3da598",
+  "buildTime": "2025-08-30T13:41:34.415Z",
+  "buildDate": "30.08.2025, 16:41:34",
+  "buildTimestamp": 1756561294415,
+  "commitHash": "9007fb5",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.2.13"
+  "version": "0.2.14"
 };
 
 export default BUILD_INFO;
