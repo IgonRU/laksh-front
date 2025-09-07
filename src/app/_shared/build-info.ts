55ee449bc7421838452b477eca6e293f2c58dd04
Igon <mtgsale@mail.ru>
2025-09-07 12:00:18 +0300

    Fix

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-09-07T07:31:42.723Z",
-  "buildDate": "07.09.2025, 10:31:42",
-  "buildTimestamp": 1757230302723,
-  "commitHash": "9b62500",
+  "buildTime": "2025-09-07T09:00:19.091Z",
+  "buildDate": "07.09.2025, 12:00:19",
+  "buildTimestamp": 1757235619091,
+  "commitHash": "d96ca6e",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.3.8"
+  "version": "0.3.9"
 };
 
 export default BUILD_INFO;
