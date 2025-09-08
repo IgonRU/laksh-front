0a1d5ea2690070882ca4bd5d7f5a3048def83127
Igon <mtgsale@mail.ru>
2025-09-08 21:02:29 +0300

    Interceptor remove

--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-09-08T17:43:15.769Z",
-  "buildDate": "08.09.2025, 20:43:15",
-  "buildTimestamp": 1757353395769,
-  "commitHash": "19aaf74",
+  "buildTime": "2025-09-08T18:02:29.758Z",
+  "buildDate": "08.09.2025, 21:02:29",
+  "buildTimestamp": 1757354549758,
+  "commitHash": "506d242",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.3.11"
+  "version": "0.3.12"
 };
 
 export default BUILD_INFO;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { IgonHttpConfig, IgonHttpErrorService } from '@igon/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withI18nSupport } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { NoCacheInterceptor } from './_interceptors/no-cache.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,11 +24,6 @@ export const appConfig: ApplicationConfig = {
         debugMode: !environment.production,
       }),
     },
-    IgonHttpErrorService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: NoCacheInterceptor,
-      multi: true
-    }
+    IgonHttpErrorService
   ]
 };
