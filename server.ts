0a1863943fd69e41d61c42cacb69db7bf073247f
Igon <mtgsale@mail.ru>
2025-09-08 21:54:52 +0300

    Fix SSR index file issue

--- server.ts
+++ server.ts
@@ -19,10 +19,9 @@ export function app(): express.Express {
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
+  // Serve static files from /browser (except HTML files)
+  server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y',
-    index: 'index.html',
   }));
 
   // All regular routes use the Angular engine
--- src/app/_shared/build-info.ts
+++ src/app/_shared/build-info.ts
@@ -2,13 +2,13 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-09-08T18:10:47.364Z",
-  "buildDate": "08.09.2025, 21:10:47",
-  "buildTimestamp": 1757355047364,
-  "commitHash": "0a1d5ea",
+  "buildTime": "2025-09-08T18:54:52.736Z",
+  "buildDate": "08.09.2025, 21:54:52",
+  "buildTimestamp": 1757357692736,
+  "commitHash": "aaafa7d",
   "branch": "master",
   "lastTag": "no-tag",
-  "version": "0.3.13"
+  "version": "0.3.14"
 };
 
 export default BUILD_INFO;
